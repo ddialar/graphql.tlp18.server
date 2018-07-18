f0e0958c0fefd85a99a3257dd2a497b57780a1a3
ddialar <ddialar@gmail.com>
2018-07-18 11:10:28 +0100

    tests(graphql.models.movie.type.test): Implement the testing suite skeleton.

--- tests/graphql/models/movie.type.test.js
+++ tests/graphql/models/movie.type.test.js
@@ -1,1 +1,42 @@
-// TODO: Implement the testing suite.
+import 'jest';
+import * as graphql from 'graphql';
+// TODO: Import all needed models.
+
+// TODO: Define the allowed fields for this model.
+// const allowedFields = [...];
+
+describe('[ GraphQL ] - Testing \'MovieType\' ...', () => {
+    test('fields integrity.', () => {
+        // TODO: Get model fields.
+        // let modelFields = ...
+
+        // TODO: Check that MovieType is an instance of GraphQL Object Type.
+
+        // TODO: Check if the model has the property 'id'.
+        // TODO: Check if the property 'id' is GraphQL Integer type.
+        // TODO: Check if the model has the property 'title'.
+        // TODO: Check if the property 'title' is GraphQL String type.
+        // TODO: Check if the model has the property 'description'.
+        // TODO: Check if the property 'description' is GraphQL String type.
+        // TODO: Check if the model has the property 'poster_image'.
+        // TODO: Check if the property 'poster_image' is GraphQL String type.
+        // TODO: Check if the model has the property 'duration'.
+        // TODO: Check if the property 'duration' is GraphQL Integer type.
+        // TODO: Check if the model has the property 'rating'.
+        // TODO: Check if the property 'rating' is GraphQL Float type.
+        // TODO: Check if the model has the property 'classification'.
+        // TODO: Check if the property 'classification' is GraphQL String type.
+        // TODO: Check if the model has the property 'year'.
+        // TODO: Check if the property 'year' is GraphQL String type.
+        // TODO: Check if the model has the property 'directors'.
+        // TODO: Check if the property 'directors' is GraphQL List of Director types.
+        // TODO: Check if the model has the property 'writers'.
+        // TODO: Check if the property 'writers' is GraphQL List of Writer types.
+        // TODO: Check if the model has the property 'actors'.
+        // TODO: Check if the property 'actors' is GraphQL List of Actor types.
+        // TODO: Check if the model has the property 'genres'.
+        // TODO: Check if the property 'genres' is GraphQL List of Genre types.
+        // TODO: Check if the model has the property 'genresAsArray'.
+        // TODO: Check if the property 'genresAsArray' is GraphQL List of GraphQL String types.
+    });
+});
