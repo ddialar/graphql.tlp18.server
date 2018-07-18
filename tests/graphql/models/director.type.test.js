15191ba8b2c8986b85868922dea6439a847e9410
ddialar <ddialar@gmail.com>
2018-07-18 11:09:41 +0100

    tests(graphql.models.director.type.test): Implement the testing suite skeleton.

--- tests/graphql/models/director.type.test.js
+++ tests/graphql/models/director.type.test.js
@@ -1,1 +1,25 @@
-// TODO: Implement the testing suite.
+import 'jest';
+import * as graphql from 'graphql';
+import DirectorType from '../../../src/graphql/models/director.type';
+import MovieType    from '../../../src/graphql/models/movie.type';
+
+// TODO: Define the allowed fields for this model.
+// const allowedFields = [...];
+
+describe('[ GraphQL ] - Testing \'DirectorType\' ...', () => {
+    test('fields integrity.', () => {
+        // TODO: Get model fields.
+        // let modelFields = ...
+
+        // TODO: Check that DirectorType is an instance of GraphQL Object Type.
+
+        // TODO: Check if the model has the property 'id'.
+        // TODO: Check if the property 'id' is GraphQL Integer type.
+        // TODO: Check if the model has the property 'name'.
+        // TODO: Check if the property 'name' is GraphQL String type.
+        // TODO: Check if the model has the property 'picture'.
+        // TODO: Check if the property 'picture' is GraphQL String type.
+        // TODO: Check if the model has the property 'movies'.
+        // TODO: Check if the property 'movies' is GraphQL List of Movie types.
+    });
+});
